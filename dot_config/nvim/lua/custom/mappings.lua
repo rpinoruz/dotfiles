fabe22742b355813d98a361184d6b61f6a783330
rpinoruz <ricpiruz@gmail.com>
2022-06-25 11:43:51 -0500

    Update directory shortcuts

--- dot_config/nvim/lua/custom/mappings.lua
+++
@@ -1,9 +0,0 @@
-local map = require("core.utils").map
-local opts = { silent = false }
-map('n', 'Y', 'y$')
-
-map('', '<space>', '/', opts)
-map('', '<C-space>', '?', opts)
-
--- Faster search and replace
-map('n', '<leader>s', ':%s//g<Left><Left>', opts)
--- dot_config/nvim/lua/custom/plugins/init.lua
+++
@@ -1,6 +0,0 @@
-return {
-   { "tpope/vim-surround" },
-   { "tpope/vim-fugitive" },
-   { "tpope/vim-repeat" },
-   { "williamboman/nvim-lsp-installer" },
- }
